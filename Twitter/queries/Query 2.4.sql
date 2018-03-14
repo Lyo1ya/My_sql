302cf76a2e0900c9fd46e9dbf788ecb8ab95a4a7
Lyo1ya <olga.v.puzenko@gmail.com>
2018-03-14 22:28:11 +0200

    Update Query 2.4.sql

--- Twitter/queries/Query 2.4.sql
+++ Twitter/queries/Query 2.4.sql
@@ -1,6 +1,6 @@
-SELECT user.nick_name 
-FROM user 
-join follower 
-on user.id = follower.followed_id
-group by user.nick_name
+SELECT u.nick_name as user
+FROM user as u
+join follower as f
+on u.id = f.followed_id
+group by user
 having count(follower_id) > 0;
